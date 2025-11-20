843bd59f0ab2982d3cbd4c80949a3e0f4e924d0b
foramupadhyay93 <foram.upadhyay93@gmail.com>
2025-11-20 11:32:03 -0500

    first commit

---
+++ src/app/components/skills-badge/skills-badge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-badge',
+  imports: [],
+  templateUrl: './skills-badge.component.html',
+  styleUrl: './skills-badge.component.scss'
+})
+export class SkillsBadgeComponent {
+
+}
